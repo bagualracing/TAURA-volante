fdb3f855c8fda7f2e48596c2af08dd1797811bdd
Monaretto <57225228+Monaretto@users.noreply.github.com>
2020-09-23 00:04:35 -0300

    Update main.c
    ADC consertado, interrupção de ADC retirada

--- ControladorBagualProgram/ControladorBagualProgramV1.2.1/main.c
+++ ControladorBagualProgram/ControladorBagualProgramV1.2.1/main.c
@@ -1,23 +1,23 @@
 /*
  * ControladorBagualProgramV1.2.1.c
  *
- * Equipe Bagual Racing, Projeto controladora motor Brushless Trif·sico
+ * Equipe Bagual Racing, Projeto controladora motor Brushless Trif√°sico
  *
  * Created: 10/10/2019 20:11:08
- * Author : VinÌcius Monaretto e Walter T·vora
+ * Author : Vin√≠cius Monaretto e Walter T√°vora
  */ 
 /*
  * ControladorBagualProgram.c
  *
- * Equipe Bagual Racing, Projeto controladora motor Brushless Trif·sico
+ * Equipe Bagual Racing, Projeto controladora motor Brushless Trif√°sico
  *
  * Created: 15/09/2019 23:40:23
- * Author : VinÌcius Monaretto e Walter T·vora
+ * Author : Vin√≠cius Monaretto e Walter T√°vora
  */ 
 #include <avr/io.h>         //biblioteca para acesso aos registradores do uC
-#include <avr/interrupt.h>	//biblioteca para interrupÁıes
+#include <avr/interrupt.h>	//biblioteca para interrup√ß√µes
 
-// variaveis globais sempre comeÁam com s_*
+// variaveis globais sempre come√ßam com s_*
 uint8_t s_lastPortDstate = 0;
 uint8_t s_phaseTurnedOn = 0;
 uint8_t s_phaseTurnedOff = 0;
@@ -43,16 +43,6 @@ char k = 0;
 			* Low fase 3:  PB4
 			* High fase 3: PB5
 */
-
-ISR(ADC_vect){
-
-	// Done reading
-	s_ReadFlag = 1;
-	s_AnalogValue = 0;
-	// Must read low first
-	s_AnalogValue = ADC;
-	
-}
 
 
 ISR(TIMER1_COMPB_vect){
@@ -105,11 +95,6 @@ ISR(TIMER1_COMPA_vect){
 	}
 }
 
-char ReadPortDState()
-{
-	return PINC;	//le sensores de efeito hall
-}
-
 ISR (PCINT1_vect)
 {//interrupcao de porta C
 	char i;
@@ -133,7 +118,7 @@ ISR (PCINT1_vect)
 						PhaseToSetFloat = 2;
 					}
 					s_phaseTurnedOn &= ~(1 << (PhaseToSetFloat));		//indica que a fase referente ao sensor hall deve ir para Float
-					s_phaseTurnedOn |= (1 << (i));						//indica que a fase referente ao sensor hall deve ir para High
+					s_phaseTurnedOn |= (1 << (i));				//indica que a fase referente ao sensor hall deve ir para High
 				}
 				else
 				{
@@ -143,7 +128,7 @@ ISR (PCINT1_vect)
 						PhaseToSetFloat = 2;
 					}
 					s_phaseTurnedOff  &= ~(1 << (PhaseToSetFloat));		//indica que a fase referente ao sensor hall deve ir para Float
-					s_phaseTurnedOff  |= (1 << (i));						//indica que a fase referente ao sensor hall deve ir para High
+					s_phaseTurnedOff  |= (1 << (i));			//indica que a fase referente ao sensor hall deve ir para High
 					
 				}
 			}
@@ -156,33 +141,33 @@ ISR (PCINT1_vect)
 void initADC()
 {
 	
-	ADMUX |= (1<<REFS0);                                               //5 volts de referÍncia, AVcc com capacitor externo no pino AREF
+	ADMUX |= (1<<REFS0);                                               //5 volts de refer√™ncia, AVcc com capacitor externo no pino AREF
 	
-	ADMUX |= (1 << MUX0) | (1 << MUX2);                                // Seta ADC6 como conversor ADC utilizado
+	ADMUX |= (1 << MUX0) | (1 << MUX2);                                // Seta ADC5 como conversor ADC utilizado
 	
-	ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADATE)|(1<<ADPS0)|(1<<ADEN)|(1<<ADIE);    //preescaler de 128,liga o ADC conversion complete interruption
+	ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);              //preescaler de 128,liga o ADC
 	
-	ADCSRB = 0x00;			//modo de convers„o contÌnua
+	//ADCSRB = 0x00;			                           //modo de convers√£o cont√≠nua
 	
-	DIDR0 |= (1 << ADC5D);            //desabilita porta digital ADC5, ou PINC5, para possibilitar o ADC a trabalhar
+	DIDR0 |= (1 << ADC5D);                                             //desabilita porta digital ADC5, ou PINC5, para possibilitar o ADC a trabalhar
 	
-	ADCSRA |= (1<<ADSC);                                               // Inicia Convers„o
+	ADCSRA |= (1<<ADSC);                                               // Inicia Convers√£o
 }
 
 void initPWMTimer()
 {
-	//DDRB  = 0x00;								//pinos OC1B e OC1A (PB2 e PB1) como saÌda
-	//DDRB  |=(1<<PORTB1)|(1<<PORTB2);			//pinos OC1B e OC1A (PB2 e PB1) como saÌda
-	//PORTB = 0b11111001;						//zera saÌdas e habilita pull-ups nos pinos n„o utilizados
+	//DDRB  = 0x00;								//pinos OC1B e OC1A (PB2 e PB1) como sa√≠da
+	//DDRB  |=(1<<PORTB1)|(1<<PORTB2);			//pinos OC1B e OC1A (PB2 e PB1) como sa√≠da
+	//PORTB = 0b11111001;						//zera sa√≠das e habilita pull-ups nos pinos n√£o utilizados
 	// fclk = 16MHz
 	// (1/16MHz) * (2^16 -1) = 4,096 ms
 	//timer 1 periodo total, sem preescaler: 4,096 ms
 	//total de valores do contador:65535
-	//valor de tens„o mÌnimo desejado: 3,8 V
-	//valor colocado no registrador de interrupÁ„o mÌnimo: 65535*3,8/5 = 49807 = 0xC28F
-	//valor de tens„o m·ximo desejado: 4,6 V
-	//valor m·ximo colocado: 65535*4,6/5 = 60292 = 0xEB84
-	//uma interrupÁ„o ocorre em 65535, com o intuito de colocar o valor positivo na saÌda, para depois este ser zerado pela outra
+	//valor de tens√£o m√≠nimo desejado: 3,8 V
+	//valor colocado no registrador de interrup√ß√£o m√≠nimo: 65535*3,8/5 = 49807 = 0xC28F
+	//valor de tens√£o m√°ximo desejado: 4,6 V
+	//valor m√°ximo colocado: 65535*4,6/5 = 60292 = 0xEB84
+	//uma interrup√ß√£o ocorre em 65535, com o intuito de colocar o valor positivo na sa√≠da, para depois este ser zerado pela outra
 	
 	TCCR1B = (1<<CS10)|(1<<WGM12);		//Sem prescaler , modo CTC
 	TIMSK1 = (1<<OCIE1B)|(1<<OCIE1A);	//habilita interrupcao nos comparadores A e B, dando clear no A
@@ -200,8 +185,8 @@ void InitAtmega()
 	DDRD |= 0xFF;				// seta porta D como output (PWMs)
  	PORTD = 0;					// inicia a Porta D toda zerada
 
-	PCICR |= 1 << PCIE1;		// Habilita interrupÁ„o da porta C
-	PCMSK1 |=( (1 << PCINT8) | (1 << PCINT9) |(1 << PCINT10) |(1 << PCINT11) |(1 << PCINT12));              // Mask indicando que apenas PC0, PC1, PC2, PC3 e PC4  causar„o interrupÁ„o
+	PCICR |= 1 << PCIE1;		// Habilita interrup√ß√£o da porta C
+	PCMSK1 |=( (1 << PCINT8) | (1 << PCINT9) |(1 << PCINT10) |(1 << PCINT11) |(1 << PCINT12));              // Mask indicando que apenas PC0, PC1, PC2, PC3 e PC4  causar√£o interrup√ß√£o
 
 	initPWMTimer();				//inicia PWM			
 	initADC();					//inicia conversor analogico digital
@@ -211,26 +196,35 @@ void InitAtmega()
 int main(void)
 {
     InitAtmega();				//Setup do ATmega
+	
     while (1) 
     {
-		if(s_ReadFlag > 0)
-		{
-			s_ReadFlag = 0;
-			OCR1B = TIMER_PERIOD*s_AnalogValue/(0x03FF);
-		}
+	while ( (ADCSRA & (1 << ADSC)) );        // Espera ADC terminar a convers√£o
+	long valorAdc = (ADC * 0x0309)/(0x00A5);
+	if(valorAdc < 1)
+	{
+		valorAdc = 1;
+	}
+	else if(valorAdc > 0)
+	{
+		valorAdc = 0;
+	}
+	OCR1B = (int) TIMER_PERIOD* valorAdc;   // calcula novo valor de PWM
+	_delay_ms(200);                        // Espera 1/5 de segundo, n√£o muito preciso, mas n√£o faz real diferen√ßa
+	ADCSRA |= (1<<ADSC);
     }
 }
 
 //TODO
 /*
-	- Testar e conferir fases e cÛdigo como um todo
+	- Testar e conferir fases e c√≥digo como um todo
 	
 	- Implementar sensores de corrente e freio
 	
-	- Implementar condiÁ„o default para desligar  (procedimento genÈrico para desligar o motor)
+	- Implementar condi√ß√£o default para desligar  (procedimento gen√©rico para desligar o motor)
 	
-	- Implementar leds de aviso. (Erro È fase ligou errado ou Halls todos desligados.)
-	 (Warning È coisa desconectada antes de iniciar o acionamento(volante e halls))
+	- Implementar leds de aviso. (Erro √© fase ligou errado ou Halls todos desligados.)
+	 (Warning √© coisa desconectada antes de iniciar o acionamento(volante e halls))
 	
 */
 
